9e5b290fb8b4d62dc9f6dbd74249d15631391ded
bryewalks <bryewalks@gmail.com>
2019-02-20 14:31:23 -0600

    local changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Fridge Items</h1>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Axios from 'axios'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
