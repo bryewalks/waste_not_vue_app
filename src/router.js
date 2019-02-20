db33948ec34f29142effa62794e428debfb64b2e
bryewalks <bryewalks@gmail.com>
2019-02-20 16:40:17 -0600

    working index and show page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import FridgeItemsIndex from './views/FridgeItemsIndex.vue'
+import FridgeItemsShow from './views/FridgeItemsShow.vue'
 
 Vue.use(Router)
 
@@ -8,18 +11,9 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', name: 'home', component: Home},
+    { path: '/about', name: 'about', component: About},
+    { path: '/fridge_items/', name: 'fridge-items-index', component: FridgeItemsIndex},
+    { path: '/fridge_items/:id', name: 'fridge-items-show', component: FridgeItemsShow}
   ]
 })
