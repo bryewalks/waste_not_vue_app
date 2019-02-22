7705ca3f4f984852a34d503bc9f8516d31dc9c7d
Tacoholic <solischristian@gmail.com>
2019-02-22 17:12:56 -0600

    auth

--- src/router.js
+++ src/router.js
@@ -6,6 +6,10 @@ import FridgeItemsIndex from './views/FridgeItemsIndex.vue'
 import FridgeItemsNew from './views/FridgeItemsNew.vue'
 import FridgeItemsShow from './views/FridgeItemsShow.vue'
 import FridgeItemsEdit from './views/FridgeItemsEdit.vue'
+import Signup from "./views/Signup.vue"
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
+
 
 Vue.use(Router)
 
@@ -14,9 +18,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     { path: '/', name: 'home', component: FridgeItemsIndex},
+    { path: '/about', name: 'about', component: About},
     { path: '/fridge_items', name: 'fridge-items-index', component: FridgeItemsIndex},
     { path: '/fridge_items/new', name: 'fridge-items-new', component: FridgeItemsNew },
     { path: '/fridge_items/:id', name: 'fridge-items-show', component: FridgeItemsShow },
-    { path: '/fridge_items/:id/edit', name: 'fridge-items-edit', component: FridgeItemsEdit }
+    { path: '/fridge_items/:id/edit', name: 'fridge-items-edit', component: FridgeItemsEdit },
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout }
+
   ]
 })
